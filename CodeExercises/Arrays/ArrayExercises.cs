43c6b7d542a6e39ce94f323befc82168ea593590
GaelLouage <Gael.Louage@hotmail.com>
2023-12-29 07:05:41 +0100

    SeperateOddAndEvenNumbers

--- CodeExercises/Arrays/ArrayExercises.cs
+++ CodeExercises/Arrays/ArrayExercises.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace CodeExercises.Arrays
 {
@@ -39,5 +41,59 @@ namespace CodeExercises.Arrays
         }
         public static Dictionary<int, int> GenerateElementFrequencyMapLinq(int[] array) => array.GroupBy(x => x).ToDictionary(x => x.Key, c => c.Count());
         #endregion
+        #region GetMinAndMaxValueOfArray
+        /*Write a C# Sharp program to find the maximum and minimum elements in an array.
+        Test Data :
+        Input the number of elements to be stored in the array :3
+        Input 3 elements in the array :
+        element - 0 : 45
+        element - 1 : 25
+        element - 2 : 21
+        Expected Output :
+        Maximum element is : 45
+        Minimum element is : 21*/
+        public static (int Min, int Max) GetMinAndMaxValueLinq(int[] array) => (array.Min(), array.Max());
+        public static (int Min, int Max) GetMinAndMaxValue(int[] array)
+        {
+            int max = Int32.MinValue;
+            int min = Int32.MaxValue;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (max < array[i]) max = array[i];
+                if (min > array[i]) min = array[i];
+            }
+            return (min, max);
+        }
+        #endregion
+        #region OddAndEven
+        /*Write a program in C# Sharp to separate odd and even integers into separate arrays.
+        Test Data :
+        Input the number of elements to be stored in the array :5
+        Input 5 elements in the array :
+        element - 0 : 25
+        element - 1 : 47
+        element - 2 : 42
+        element - 3 : 56
+        element - 4 : 32
+        Expected Output:
+        The Even elements are:
+        42 56 32
+        The Odd elements are :
+        25 47*/
+        public static (List<int> Odd, List<int> Even) SeperateOddAndEvenNumbers(int[] array)
+        {
+            var odd = new List<int>();
+            var even = new List<int>();
+            foreach (var num in array)
+            {
+                if (num % 2 is 0) even.Add(num);
+                if (num % 2 is not 0) odd.Add(num);
+            }
+            return (odd, even);
+        }
+        public static (List<int> Odd, List<int> Even) SeperateOddAndEvenNumbersLinq(int[] array) =>
+         (array.Where(x => x % 2 != 0).ToList(), array.Where(x => x % 2 == 0).ToList());
+        #endregion
+
     }
 }
